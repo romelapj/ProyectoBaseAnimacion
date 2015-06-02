32b2176402067048f4d00f1843158ef7f77e84e7
Romel Palomino Jaramillo <romelpalomino@hotmail.com>
2015-06-02 14:30:24 -0500

    Arreglos en el toolbar y agregar menu derecho

--- app/src/main/java/com/romelapj/proyectobaseanimacion/MainActivity.java
+++ app/src/main/java/com/romelapj/proyectobaseanimacion/MainActivity.java
@@ -2,22 +2,39 @@ package com.romelapj.proyectobaseanimacion;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 
 public class MainActivity extends ActionBarActivity {
-
+    private MenuItem inboxMenuItem;
+    @InjectView(R.id.tbMain)
+    Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        setupToolbar();
+    }
+
+    private void setupToolbar() {
+        setSupportActionBar(toolbar);
+        toolbar.setTitle("");
+        toolbar.setNavigationIcon(R.mipmap.ic_menu_white);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        inboxMenuItem = menu.findItem(R.id.action_mood);
+        inboxMenuItem.setActionView(R.layout.item_menu_right);
         return true;
     }
 
@@ -29,7 +46,7 @@ public class MainActivity extends ActionBarActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_mood) {
             return true;
         }
 
